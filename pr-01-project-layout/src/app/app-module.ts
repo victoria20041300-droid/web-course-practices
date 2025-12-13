b9c00a3c877bbe227a7ee037c7aaaea29281557f
victoria <victoria20041300@gmail.com>
2025-12-13 13:09:42 +0200

    commit1

---
+++ pr-01-project-layout/src/app/app-module.ts
@@ -0,0 +1,28 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Header } from './header/header';
+import { Footer } from './footer/footer';
+import { Sidebar } from './sidebar/sidebar';
+import { MainContent } from './main-content/main-content';
+
+@NgModule({
+  declarations: [
+    App,
+    Header,
+    Footer,
+    Sidebar,
+    MainContent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
---
+++ pr-01-project-layout/src/app/main-content/main-content.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-content',
+  standalone: false,
+  templateUrl: './main-content.html',
+  styleUrl: './main-content.css',
+})
+export class MainContent {
+
+}
