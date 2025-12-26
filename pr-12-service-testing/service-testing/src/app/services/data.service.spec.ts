c7702d4cfc8215fe01bb5128b8878a7a754ad459
victoria <victoria20041300@gmail.com>
2025-12-26 14:42:43 +0200

    pr-12: add unit tests for DataService with HttpClient mocking

---
+++ pr-12-service-testing/service-testing/src/app/services/data.service.spec.ts
@@ -0,0 +1,45 @@
+import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
+import { DataService } from './data.service';
+
+describe('DataService', () => {
+  let service: DataService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [DataService]
+    });
+
+    service = TestBed.inject(DataService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should fetch users via GET request', () => {
+    const mockUsers = [
+      { id: 1, name: 'John' },
+      { id: 2, name: 'Alice' }
+    ];
+
+    service.getUsers().subscribe(users => {
+      expect(users).toEqual(mockUsers);
+    });
+
+    const req = httpMock.expectOne('https://jsonplaceholder.typicode.com/users');
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockUsers);
+  });
+});
